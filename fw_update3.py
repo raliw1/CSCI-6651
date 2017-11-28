9fe81665f809c45d2db5e8477cbebfbf6a2546c7
raliw1 <34015346+raliw1@users.noreply.github.com>
2017-11-27 20:51:58 -0500

    Add files via upload

---
+++ fw_update3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.6
+import sqlite3
+import os
+
+# define the database location
+mydb = '/var/tmp/ip.db'
+
+# read entries in db
+db = sqlite3.connect(mydb)
+c = db.execute('select ip from Hostile')
+for row in c:
+    ip = c.fetchone()	# returns a tuple
+    blockip = "ipset add blacklist " + ip[0]
+    #print(blockip)
+    ret = os.system(blockip)
+    print(ret)
